437b2f317558c21cee4b16cd5646253c66eef7e5
Ian <ian@g.ucla.edu>
2023-07-13 22:29:08 -0700

    WHAT: 	- installed react-native-community/eslint-config 	- installed eslint 	- installed eslint-plugin-oretter 	- set up eslint check script: yarn lint 	- set up eslint rules in file ".eslintrc" 	- installed react-native-paper

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text style={styles.text}>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text style={styles.text}>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 },
+
+  search: {
+    backgroundColor: "green",
+  },
+
+  list: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+  },
+
+  text: {
+    padding: 16,
   },
 });
