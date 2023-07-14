d77b19330976a33404f0d9463032fa2ad91e2a4f
Ian <ian@g.ucla.edu>
2023-07-14 00:03:35 -0700

    WHAT: 	- Added Restaurant Info Card 	- Card displays photo and restaurant name

--- App.js
+++ App.js
@@ -1,47 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { Searchbar } from "react-native-paper";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from "react-native";
-
-const isAndroid = Platform.OS === "android";
+import { RestaurantSceen } from "./src/features/restaurants/screens/restaurant.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search" />
-        </View>
-        <View style={styles.list}>
-          <Text style={styles.text}>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantSceen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 },
-
-  search: {
-    backgroundColor: "white",
-    padding: 10,
-  },
-
-  list: {
-    flex: 1,
-    backgroundColor: "blue",
-  },
-
-  text: {
-    padding: 16,
-  },
-});
---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 SomeRandom St",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporary,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Card.Content>
+        <Text>{name}</Text>
+      </Card.Content>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+});
