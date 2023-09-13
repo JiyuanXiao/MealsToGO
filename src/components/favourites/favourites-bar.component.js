cce17fc21672c6d514d204e288314915b3da6524
Ian <ian@g.ucla.edu>
2023-09-13 16:13:26 +0800

    WHAT: 	- Made favourites bar restaurants touchable and show 	  the restaurant detail 	- store favourites list in phone locally by using 	  "react-native-async-storage/async-storage"

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,21 +1,32 @@
 import React from "react";
 import stytled from "styled-components/native";
-import { ScrollView } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
 import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
 import { Spacer } from "../spacer/spacer.component";
+import { Text } from "../typography/text.component";
 
 const FacouritesWarpper = stytled.View`
     padding: 10px;
 `;
-export const FavouritesBar = ({ favourites }) => {
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
   return (
     <FacouritesWarpper>
+      <Spacer variant="left.large">
+        <Text variant="caption">Favoutites</Text>
+      </Spacer>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {favourites.map((restaurant) => {
           const key = restaurant.name;
           return (
             <Spacer key={key} position="left" size="medium">
-              <CompactRestaurantInfo restaurant={restaurant} />
+              <TouchableOpacity
+                onPress={() => onNavigate("RestaurantDetail", { restaurant })}
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
             </Spacer>
           );
         })}
