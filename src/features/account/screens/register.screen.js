8d27b125cf20bfa58aa51f5f23eb656d312637c1
Ian <ian@g.ucla.edu>
2023-10-01 23:20:52 -0700

    WHAT: 	- Set up register function 	- Finished Register Screen

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,23 +1,76 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import {
   AccountBackground,
   AccountCover,
   AccountContainer,
   AuthButton,
+  AuthInput,
+  Title,
+  ErrorContainer,
 } from "../components/account.styles";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatedPassowrd, setRepeatedPassword] = useState("");
+  const { onRegister, error } = useContext(AuthenticationContext);
+  console.log(error);
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
         <Spacer size="large">
-          <AuthButton mode="contained" onPress={() => navigation.goBack()}>
-            Back
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        <Spacer size="large">
+          <AuthInput
+            label="Repeat Password"
+            value={repeatedPassowrd}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(p) => setRepeatedPassword(p)}
+          />
+        </Spacer>
+        {error && (
+          <ErrorContainer size="large">
+            <Text variant="error">{error}</Text>
+          </ErrorContainer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="email-outline"
+            mode="contained"
+            onPress={() => onRegister(email, password, repeatedPassowrd)}
+          >
+            Register
           </AuthButton>
         </Spacer>
       </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
     </AccountBackground>
   );
 };
--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,5 +1,5 @@
 import React, { useState, createContext } from "react";
-import { loginRequest } from "./authentication.service";
+import { loginRequest, registerRequest } from "./authentication.service";
 
 export const AuthenticationContext = createContext();
 
@@ -20,9 +20,34 @@ export const AuthenticationContextProvider = ({ children }) => {
         setError(e.toString());
       });
   };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    setIsLoading(true);
+    registerRequest(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
   return (
     <AuthenticationContext.Provider
-      value={{ isAuthenticated: !!user, user, isLoading, error, onLogin }}
+      value={{
+        isAuthenticated: !!user,
+        user,
+        isLoading,
+        error,
+        onLogin,
+        onRegister,
+      }}
     >
       {children}
     </AuthenticationContext.Provider>
