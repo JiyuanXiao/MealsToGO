0b69a8cac5efb135dc8c7392b5aeecc9f6281fe3
Ian <ian@g.ucla.edu>
2023-10-14 00:18:16 -0700

    WHAT: 	- Delete CameraScreen and PhotoReviewScreen 	- Installed expo-image-picker 	- Access device camera and device photos by using expo-image-picker 	- Check camera access permission by using expo-camera's method 	  useCameraPermission();

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
-import { TouchableOpacity } from "react-native";
-import { List, Avatar } from "react-native-paper";
+import { TouchableOpacity, Alert } from "react-native";
+import { List, Avatar, Modal, Portal, PaperProvider } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
+import { Camera } from "expo-camera";
 import styled from "styled-components/native";
 import { SafeArea } from "../../../components/utility/safr-area.component";
 import { Text } from "../../../components/typography/text.component";
@@ -25,14 +27,85 @@ const LogoutIcon = (props) => (
   <List.Icon {...props} color="black" icon="door" />
 );
 
+const CameraIcon = (props) => (
+  <List.Icon {...props} color="white" icon="camera" />
+);
+
+const AlbumsIcon = (props) => (
+  <List.Icon {...props} color="white" icon="image" />
+);
+
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
   const [photo, setPhoto] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [cameraPermission, requestCameraPermission] =
+    Camera.useCameraPermissions();
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {
+    backgroundColor: "black",
+    padding: 10,
+  };
+
   const getProfilePhoto = async (currentUser) => {
     const photoUri = await AsyncStorage.getItem(
       `${currentUser.uid}-photo-reviewed`
     );
     setPhoto(photoUri);
+  };
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      AsyncStorage.setItem(`${user.uid}-photo-reviewed`, result.assets[0].uri);
+      hideModal();
+    }
+  };
+
+  const takePhoto = async () => {
+    if (!cameraPermission.granted) {
+      Alert.alert(
+        "Access Permission Issue",
+        "Please allow camera access in device settings",
+        [
+          {
+            text: "Cancel",
+            onPress: () => {
+              return;
+            },
+            style: "cancel",
+          },
+          {
+            text: "Ask Permission",
+            onPress: requestCameraPermission,
+          },
+        ]
+      );
+      return;
+    }
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      AsyncStorage.setItem(`${user.uid}-photo-reviewed`, result.assets[0].uri);
+      hideModal();
+    }
   };
 
   useFocusEffect(() => {
@@ -41,28 +114,55 @@ export const SettingsScreen = ({ navigation }) => {
 
   return (
     <SafeArea>
-      <AvatarContainer>
-        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-          {photo ? (
-            <Avatar.Image size={180} source={{ uri: photo }} />
-          ) : (
-            <Avatar.Icon size={180} icon="human" backgorundColor="#2182BD" />
-          )}
-          <Spacer postion="top" size="large">
-            <Text variant="label">{user.email}</Text>
-          </Spacer>
-        </TouchableOpacity>
-      </AvatarContainer>
+      <PaperProvider>
+        <AvatarContainer>
+          <TouchableOpacity onPress={showModal}>
+            {photo ? (
+              <Avatar.Image size={180} source={{ uri: photo }} />
+            ) : (
+              <Avatar.Icon size={180} icon="human" backgorundColor="#2182BD" />
+            )}
+            <Spacer postion="top" size="large">
+              <Text variant="label">{user.email}</Text>
+            </Spacer>
+          </TouchableOpacity>
+        </AvatarContainer>
+        <Portal>
+          <Modal
+            visible={visible}
+            onDismiss={hideModal}
+            contentContainerStyle={containerStyle}
+          >
+            <List.Section>
+              <List.Subheader>Change Profile Photo</List.Subheader>
+              <TouchableOpacity onPress={takePhoto}>
+                <List.Item
+                  title="Take Photo"
+                  titleStyle={{ color: "white" }}
+                  left={CameraIcon}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={pickImage}>
+                <List.Item
+                  title="Choose From Albums"
+                  titleStyle={{ color: "white" }}
+                  left={AlbumsIcon}
+                />
+              </TouchableOpacity>
+            </List.Section>
+          </Modal>
+        </Portal>
 
-      <List.Section>
-        <SettingsItem
-          title="Favourites"
-          description="View your favourites"
-          left={FavouritesIcon}
-          onPress={() => navigation.navigate("Favourites")}
-        />
-        <SettingsItem title="Logout" left={LogoutIcon} onPress={onLogout} />
-      </List.Section>
+        <List.Section>
+          <SettingsItem
+            title="Favourites"
+            description="View your favourites"
+            left={FavouritesIcon}
+            onPress={() => navigation.navigate("Favourites")}
+          />
+          <SettingsItem title="Logout" left={LogoutIcon} onPress={onLogout} />
+        </List.Section>
+      </PaperProvider>
     </SafeArea>
   );
 };
--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -6,8 +6,6 @@ import {
 
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
-import { CameraScreen } from "../../features/settings/screens/camera.screen";
-import { PhotoReviewScreen } from "../../features/settings/screens/photo-review.screen";
 
 const SettingsStack = createStackNavigator();
 
@@ -24,8 +22,6 @@ export const SettingsNavigator = ({ route, navigation }) => {
         component={SettingsScreen}
       />
       <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
-      <SettingsStack.Screen name="Camera" component={CameraScreen} />
-      <SettingsStack.Screen name="PhotoReview" component={PhotoReviewScreen} />
     </SettingsStack.Navigator>
   );
 };
